6b786b0f648a6b011fe1bdd721206da7d92faa63
Tony Arcieri <tony@iqlusion.io>
2018-07-15 09:16:26 -0700

    rustfmt 0.8.2-nightly formatting fixups

--- keyuri/src/lib.rs
+++ keyuri/src/lib.rs
@@ -3,7 +3,13 @@
 #![crate_name = "keyuri"]
 #![crate_type = "rlib"]
 #![allow(unknown_lints, suspicious_arithmetic_impl)]
-#![deny(warnings, missing_docs, unsafe_code, unused_import_braces, unused_qualifications)]
+#![deny(
+    warnings,
+    missing_docs,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications
+)]
 #![doc(html_root_url = "https://docs.rs/keyuri/0.0.0")]
 
 extern crate clear_on_drop;
