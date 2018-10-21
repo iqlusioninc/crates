f45fc907ef1e327d0fc2e1a997bf0bb724c630c1
Tony Arcieri <tony@iqlusion.io>
2018-10-21 00:28:43 -0700

    gaunt v0.0.1

--- gaunt/src/lib.rs
+++ gaunt/src/lib.rs
@@ -9,7 +9,7 @@
     unused_qualifications,
 )]
 #![forbid(unsafe_code)]
-#![doc(html_root_url = "https://docs.rs/gaunt/0.0.0")]
+#![doc(html_root_url = "https://docs.rs/gaunt/0.0.1")]
 
 extern crate failure;
 #[macro_use]
@@ -30,4 +30,4 @@ pub use path::*;
 pub use response::*;
 
 /// Gaunt's default `User-Agent` string
-pub const USER_AGENT: &str = concat!("Gaunt ", env!("CARGO_PKG_VERSION"));
+pub const USER_AGENT: &str = concat!("gaunt.rs ", env!("CARGO_PKG_VERSION"));
