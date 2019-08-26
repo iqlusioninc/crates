40f07a75911bd6c287b1e82028ff2dd5598e8723
Georgios Konstantopoulos <me@gakonst.com>
2019-08-26 16:26:18 +0300

    implement CloneableSecret for String
    This requires enabling default features from Zeroize.

--- secrecy/src/string.rs
+++ secrecy/src/string.rs
@@ -1,9 +1,10 @@
 //! Secret strings
 
-use super::{DebugSecret, Secret};
+use super::{DebugSecret, Secret, CloneableSecret};
 use alloc::string::String;
 
 /// Secret strings
 pub type SecretString = Secret<String>;
 
 impl DebugSecret for String {}
+impl CloneableSecret for String {}
