018fce2b25eec9f5b752ae649f1f4d4ce0503b47
Tony Arcieri <tony@iqlusion.io>
2019-08-27 23:11:00 -0700

    secrecy/zeroize: Cleanups for `bytes` feature; remove `Bytes` impls
    - Removes the redundant `bytes` feature; imports it as `bytes`
    - Removes the `Bytes` impl for `Zeroize`
    - Removes the `SecretBytes` type
    
    I will open another PR with a proper solution for `Bytes`. It's a
    particularly tricky problem, but one I think has an effective and safe
    solution. This PR is just to clear the dust in advance.

--- secrecy/src/bytes.rs
+++ secrecy/src/bytes.rs
@@ -1,14 +1,10 @@
-//! Secret bytes
+//! Optional `Secret` wrapper type for the `bytes::BytesMut` crate.
 
 use super::{CloneableSecret, DebugSecret, Secret};
-use bytes_crate::{Bytes, BytesMut};
+use bytes::BytesMut;
 
-/// Secret bytes
-pub type SecretBytes = Secret<Bytes>;
-/// Secret bytes_mut
+/// Alias for `Secret<BytesMut>`
 pub type SecretBytesMut = Secret<BytesMut>;
 
-impl DebugSecret for Bytes {}
-impl CloneableSecret for Bytes {}
 impl DebugSecret for BytesMut {}
 impl CloneableSecret for BytesMut {}
