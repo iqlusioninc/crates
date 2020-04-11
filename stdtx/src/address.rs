c551bed43bba01548293d8775312437f84ab9dac
Ethan Buchman <ethan@coinculture.info>
2020-04-10 20:57:05 -0400

    stdtx: JSON encode/decode address via bech32

--- stdtx/src/address.rs
+++ stdtx/src/address.rs
@@ -2,6 +2,7 @@
 
 use crate::error::{Error, ErrorKind};
 use anomaly::ensure;
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use std::convert::TryInto;
 use subtle_encoding::bech32;
 
@@ -31,6 +32,25 @@ impl Address {
     /// Encode this address as Bech32
     pub fn to_bech32(&self, hrp: &str) -> String {
         bech32::encode(hrp, &self.0)
+    }
+}
+
+impl Serialize for Address {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let s = self.to_bech32("cosmos"); // TODO: how to generalize?
+        s.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Address {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let (_hrp, addr) =
+            Address::from_bech32(s).map_err(|e| de::Error::custom(format!("{:?}", e)))?;
+        Ok(addr)
     }
 }
 
