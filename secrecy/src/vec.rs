3a9a7138c4c76d674c15f204cb4914d6e27c9878
Tony Arcieri (iqlusion) <tony@iqlusion.io>
2020-07-08 10:08:48 -0700

    secrecy: add doc_cfg (#464)
    Improves `rustdoc` documentation by notating types which are
    conditionally available based on cargo features.

--- secrecy/src/vec.rs
+++ secrecy/src/vec.rs
@@ -1,12 +1,12 @@
 //! Secret `Vec` types
 
-use super::{DebugSecret, Secret};
+use super::{CloneableSecret, DebugSecret, Secret};
 use alloc::vec::Vec;
 use zeroize::Zeroize;
 
 /// `Vec` types containing secret value
-#[cfg(feature = "alloc")]
+#[cfg_attr(docsrs, doc(cfg(feature = "alloc")))]
 pub type SecretVec<S> = Secret<Vec<S>>;
 
-#[cfg(feature = "alloc")]
+impl<S: CloneableSecret + Zeroize> CloneableSecret for Vec<S> {}
 impl<S: DebugSecret + Zeroize> DebugSecret for Vec<S> {}
