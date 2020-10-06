b93dce17e48c215fd271f9a099f34d5d2474d38e
Tony Arcieri <tony@iqlusion.io>
2020-10-05 18:44:03 -0700

    stdtx: `bytes` fields (serialized as Base64)
    Adds support for Protobuf-style `bytes` fields, encoded as wire type 2,
    and Base64 encoded in JSON.

--- stdtx/src/msg/value.rs
+++ stdtx/src/msg/value.rs
@@ -9,6 +9,9 @@ use crate::{
 /// Message values - data contained in fields of a message
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Value {
+    /// Bytes
+    Bytes(Vec<u8>),
+
     /// `sdk.AccAddress`: Cosmos SDK account addresses
     /// <https://godoc.org/github.com/cosmos/cosmos-sdk/types#AccAddress>
     SdkAccAddress(Address),
@@ -29,6 +32,7 @@ impl Value {
     /// Get the type of this value
     pub fn value_type(&self) -> ValueType {
         match self {
+            Value::Bytes(_) => ValueType::Bytes,
             Value::SdkAccAddress(_) => ValueType::SdkAccAddress,
             Value::SdkDecimal(_) => ValueType::SdkDecimal,
             Value::SdkValAddress(_) => ValueType::SdkValAddress,
@@ -41,6 +45,7 @@ impl Value {
     pub(super) fn wire_type(&self) -> u64 {
         match self {
             // Length-delimited types
+            Value::Bytes(_) |
             Value::SdkAccAddress(_)
             | Value::SdkDecimal(_)
             | Value::SdkValAddress(_)
@@ -51,6 +56,7 @@ impl Value {
     /// Encode this value as Amino bytes
     pub(super) fn to_amino_bytes(&self) -> Vec<u8> {
         match self {
+            Value::Bytes(b) => b.clone(),
             Value::SdkAccAddress(addr) | Value::SdkValAddress(addr) => addr.as_ref().to_vec(),
             Value::SdkDecimal(decimal) => decimal.to_amino_bytes(),
             Value::String(s) => s.clone().into_bytes(),
@@ -60,6 +66,7 @@ impl Value {
     /// Encode this value as a [`serde_json::Value`]
     pub(super) fn to_json_value(&self, schema: &Schema) -> serde_json::Value {
         serde_json::Value::String(match self {
+            Value::Bytes(b) => base64::encode(b),
             Value::SdkAccAddress(addr) => addr.to_bech32(schema.acc_prefix()),
             Value::SdkDecimal(decimal) => decimal.to_string(),
             Value::SdkValAddress(addr) => addr.to_bech32(schema.val_prefix()),
@@ -71,6 +78,12 @@ impl Value {
 impl From<Decimal> for Value {
     fn from(dec: Decimal) -> Value {
         Value::SdkDecimal(dec)
+    }
+}
+
+impl From<Vec<u8>> for Value {
+    fn from(b: Vec<u8>) -> Value {
+        Value::Bytes(b)
     }
 }
 
