af5345aec56504ba825d2866535cb587b776c051
Shella Stephens <shella@iqlusion.io>
2021-04-30 08:08:36 -0700

    hkd32: `hmac` to v0.11, `pbkdf2` to v0.8

--- hkd32/src/path.rs
+++ hkd32/src/path.rs
@@ -211,7 +211,7 @@ impl<'a> Component<'a> {
 
     /// Serialize this component as a length-prefixed bytestring.
     #[cfg(feature = "alloc")]
-    pub fn to_bytes(&self) -> Vec<u8> {
+    pub fn to_bytes(self) -> Vec<u8> {
         let mut serialized = Vec::with_capacity(1 + self.len());
         serialized.push((self.len() - 1) as u8);
         serialized.extend_from_slice(&self.0);
