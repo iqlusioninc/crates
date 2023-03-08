fad8519722738ff9cb799158307e5e20c59de66b
Tony Arcieri <tony@iqlusion.io>
2023-03-08 09:41:33 -0700

    Remove `mintscan`
    It's been integrated into `cosmon`.
    
    Long-term we should migrate away from Mintscan's API in general since
    they seem to be locking it down and don't intend for it to be used by
    the public.

--- mintscan/src/v1.rs
+++
@@ -1,6 +0,0 @@
-//! `/v1` API endpoints.
-
-pub mod staking;
-mod status;
-
-pub use self::status::Status;
--- mintscan/src/v1/staking.rs
+++
@@ -1,5 +0,0 @@
-//! `/v1/staking` endpoints.
-
-pub mod validator;
-
-pub use self::validator::Validator;
